e19ff0e4b6fd67936ffc1ca9db0e47c2214cd804
Bowrna <mailbowrna@gmail.com>
2025-04-15 16:04:49 +0530

    conflict resolved

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -124,7 +124,8 @@ func initHTTPHandlers(e *echo.Echo, a *App) {
 		g.GET("/api/bounces/:id", pm(hasID(a.GetBounce), "bounces:get"))
 		g.DELETE("/api/bounces", pm(a.DeleteBounces, "bounces:manage"))
 		g.DELETE("/api/bounces/:id", pm(hasID(a.DeleteBounce), "bounces:manage"))
-
+		g.PUT("/api/bounces/blocklist", pm(a.BlocklistSubscriberBounces, "bounces:manage"))
+		g.PUT("/api/bounces/:id/blocklist", pm(a.BlocklistSubscriberBounces, "bounces:manage"))
 		// Subscriber operations based on arbitrary SQL queries.
 		// These aren't very REST-like.
 		g.POST("/api/subscribers/query/delete", pm(a.DeleteSubscribersByQuery, "subscribers:manage"))
